65d3a116a3a4c7f1b7ecf5e8ae931f68fd7b2912
Zekademi Bilişim & Yazılım <106998412+zekademicom@users.noreply.github.com>
2022-06-07 11:15:51 +0300

    Add files via upload

---
+++ src/main/java/com/zekademi/strongprettyhomes/StrongPrettyHomesApplication.java
@@ -0,0 +1,13 @@
+package com.zekademi.strongprettyhomes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrongPrettyHomesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StrongPrettyHomesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zekademi/strongprettyhomes/StrongPrettyHomesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zekademi.strongprettyhomes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrongPrettyHomesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
