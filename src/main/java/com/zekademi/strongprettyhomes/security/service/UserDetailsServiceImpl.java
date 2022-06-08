5f798873bd490f2dd8f7936dd0d970d7e4b3ed5f
adl3416 <makara3416@gmail.com>
2022-06-08 16:28:09 +0200

    d

---
+++ src/main/java/com/zekademi/strongprettyhomes/security/service/UserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package com.zekademi.strongprettyhomes.security.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+
+
+
+public class UserDetailsServiceImpl {
+    @AllArgsConstructor
+    @Service
+    public class UserDetailsServiceImpl implements UserDetailsService {
+
+        private final UserRepository userRepository;
+
+        @Override
+        @Transactional
+        public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+            User user = userRepository.findByEmail(email)
+                    .orElseThrow(() -> new UsernameNotFoundException("User Not Found with email: " + email));
+
+            return UserDetailsImpl.build(user);
+        }
+    }
+
+}
