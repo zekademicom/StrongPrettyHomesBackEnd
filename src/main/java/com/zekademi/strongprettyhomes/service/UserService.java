a4dfc5e559441c7d1ae2ef4eb124d102cee2db44
Burhan Bediz <bsmya7935@gmail.com>
2022-06-10 16:17:53 +0300

    UserSonHali

--- src/main/java/com/zekademi/strongprettyhomes/service/UserService.java
+++ src/main/java/com/zekademi/strongprettyhomes/service/UserService.java
@@ -66,13 +66,12 @@ public class UserService {
 
 
 
-    public void login(String email, String password) throws AuthException{
-        try{
+    public void login(String email, String password) throws AuthException {
+        try {
             Optional<User> user = userRepository.findByEmail(email);
-            if(!BCrypt.checkpw(password,user.get().getPassword()));
-            throw new AuthException("invalid credentials");
-        }
-        catch(Exception e){
+            if (!BCrypt.checkpw(password, user.get().getPassword()))
+                throw new AuthException("invalid credentials");
+        }catch (Exception e){
             throw new AuthException("invalid credentials");
         }
     }
