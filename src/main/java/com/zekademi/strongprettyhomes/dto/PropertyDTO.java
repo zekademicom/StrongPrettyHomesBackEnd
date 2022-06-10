16f6976291ee49558d50baa968d8fa46196b8187
theLostCode <lostcoding11@ghmail.com>
2022-06-10 11:24:53 +0300

    property created

--- src/main/java/com/zekademi/strongprettyhomes/dto/PropertyDTO.java
+++ src/main/java/com/zekademi/strongprettyhomes/dto/PropertyDTO.java
@@ -1,57 +1,57 @@
-package com.zekademi.strongprettyhomes.dto;
-
-import com.zekademi.strongprettyhomes.domain.enumeration.PropertyStatus;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import java.sql.Timestamp;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class PropertyDTO {
-
-    private Long id;
-
-    private String title;
-
-    private String description;
-
-    private String category;
-
-    private String type;
-
-    private String bedrooms;
-
-    private String bathrooms;
-
-    private String garages;
-
-    private String area;
-
-    private String price;
-
-    private String location;
-
-    private String address;
-
-    private String country;
-
-    private String city;
-
-    private String district;
-
-    private Agent agent;
-
-    private Timestamp createdDate;
-
-    private Long likes;
-
-    private Long visitCount;
-
-    private PropertyStatus status;
-
-}
+//package com.zekademi.strongprettyhomes.dto;
+//
+//import com.zekademi.strongprettyhomes.domain.enumeration.PropertyStatus;
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//
+//import java.sql.Timestamp;
+//
+//@Getter
+//@Setter
+//@NoArgsConstructor
+//@AllArgsConstructor
+//public class PropertyDTO {
+//
+//    private Long id;
+//
+//    private String title;
+//
+//    private String description;
+//
+//    private String category;
+//
+//    private String type;
+//
+//    private String bedrooms;
+//
+//    private String bathrooms;
+//
+//    private String garages;
+//
+//    private String area;
+//
+//    private String price;
+//
+//    private String location;
+//
+//    private String address;
+//
+//    private String country;
+//
+//    private String city;
+//
+//    private String district;
+//
+////    private Agent agent;
+//
+//    private Timestamp createdDate;
+//
+//    private Long likes;
+//
+//    private Long visitCount;
+//
+//    private PropertyStatus status;
+//
+//}
--- src/main/java/com/zekademi/strongprettyhomes/repository/PropertyRepository.java
+++ src/main/java/com/zekademi/strongprettyhomes/repository/PropertyRepository.java
@@ -1,29 +1,29 @@
-package com.zekademi.strongprettyhomes.repository;
-
-import com.zekademi.strongprettyhomes.dto.PropertyDTO;
-import com.zekademi.strongprettyhomes.exception.ResourceNotFoundException;
-import com.zekademi.strongprettyhomes.repository.search.CustomizedProductRepository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
-
-import javax.transaction.Transactional;
-import java.util.List;
-import java.util.Optional;
-
-@Repository
-public interface PropertyRepository extends JpaRepository<Property, Long> {
-
-    @Transactional
-    @Query("SELECT new com.zekademi.strongprettyhomes.dto.PropertyDTO(c) FROM Property c")
-    List<PropertyDTO> findAllProperty();
-
-    @Transactional
-    @Query("SELECT new com.zekademi.strongprettyhomes.dto.PropertyDTO(c) FROM Property c WHERE c.id = ?1")
-    Optional<PropertyDTO> findPropertyByIdx(Long id) throws ResourceNotFoundException;
-
-    @Transactional
-    @Query("SELECT c FROM Property c " +
-            "LEFT JOIN fetch c.image img WHERE c.id = ?1")
-    Optional<Property> findPropertyById(Long id) throws ResourceNotFoundException;
-}
+//package com.zekademi.strongprettyhomes.repository;
+//
+//import com.zekademi.strongprettyhomes.domain.Property;
+//import com.zekademi.strongprettyhomes.dto.PropertyDTO;
+//import com.zekademi.strongprettyhomes.exception.ResourceNotFoundException;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.Query;
+//import org.springframework.stereotype.Repository;
+//
+//import javax.transaction.Transactional;
+//import java.util.List;
+//import java.util.Optional;
+//
+//@Repository
+//public interface PropertyRepository extends JpaRepository<Property, Long> {
+//
+//    @Transactional
+//    @Query("SELECT new com.zekademi.strongprettyhomes.dto.PropertyDTO(c) FROM Property c")
+//    List<PropertyDTO> findAllProperty();
+//
+//    @Transactional
+//    @Query("SELECT new com.zekademi.strongprettyhomes.dto.PropertyDTO(c) FROM Property c WHERE c.id = ?1")
+//    Optional<PropertyDTO> findPropertyByIdx(Long id) throws ResourceNotFoundException;
+//
+//    @Transactional
+//    @Query("SELECT c FROM Property c " +
+//            "LEFT JOIN fetch c.image img WHERE c.id = ?1")
+//    Optional<Property> findPropertyById(Long id) throws ResourceNotFoundException;
+//}
