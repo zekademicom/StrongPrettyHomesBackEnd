822aa4630e2bbab646b28f602cb0edf999596fdb
severHatice <95497761+severHatice@users.noreply.github.com>
2022-06-11 10:00:28 +0200

    path

--- src/main/java/com/zekademi/strongprettyhomes/controller/AgentController.java
+++ src/main/java/com/zekademi/strongprettyhomes/controller/AgentController.java
@@ -17,7 +17,7 @@ import java.util.Map;
 @RestController
 @AllArgsConstructor
 @Produces(MediaType.APPLICATION_JSON)
-@RequestMapping()
+@RequestMapping(path="/agent")
 public class AgentController {
     public AgentService agentService;
 
