dcdccde427a8c510399d705c4ee440aa246034ef
Yildiz Aydogan <94450275+YildizAydogan@users.noreply.github.com>
2022-06-11 13:41:20 +0200

    Update AgentService.java

--- src/main/java/com/zekademi/strongprettyhomes/service/AgentService.java
+++ src/main/java/com/zekademi/strongprettyhomes/service/AgentService.java
@@ -1,18 +1,12 @@
 package com.zekademi.strongprettyhomes.service;
 import com.zekademi.strongprettyhomes.domain.Agent;
 import com.zekademi.strongprettyhomes.domain.AgentImage;
-<<<<<<< HEAD
-import com.zekademi.strongprettyhomes.exception.BadRequestException;
-import com.zekademi.strongprettyhomes.exception.ConflictException;
-import com.zekademi.strongprettyhomes.repository.AgentImageRepository;
-=======
 import com.zekademi.strongprettyhomes.dto.AgentDTO;
 import com.zekademi.strongprettyhomes.exception.BadRequestException;
 import com.zekademi.strongprettyhomes.exception.ConflictException;
 import com.zekademi.strongprettyhomes.exception.ResourceNotFoundException;
 import com.zekademi.strongprettyhomes.repository.AgentImageRepository;
 import com.zekademi.strongprettyhomes.repository.AgentRepository;
->>>>>>> main
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,50 +14,26 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+
+
 
 @AllArgsConstructor
 @Service
 public class AgentService {
 
-<<<<<<< HEAD
+    
+    
     private final AgentRepository agentRepository;
     private final AgentImageRepository agentImageRepository;
     private final static String IMAGE_NOT_FOUND_MSG = "image with id %s not found";
     private final static String CAR_NOT_FOUND_MSG = "car with id %d not found";
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
+    
+    
     public void updateAgent(Long id, String agentImageId, Agent agent) throws BadRequestException {
         agent.setId(id);
 
@@ -78,12 +48,10 @@ public class AgentService {
         agent.setAgentImage(agentImage);
         agentRepository.save(agent);
     }
-=======
-    static public AgentRepository agentRepository;
-    static public AgentImageRepository agentImageRepository;
-    private final static String IMAGE_NOT_FOUND_MSG = "image with id %s not found";
-    private final static String AGENT_NOT_FOUND_MSG = "agent with id %d not found";
 
+    
+    
+    
     public void createAgent(Agent agent, String imageId) throws BadRequestException {
        AgentImage agentImage = agentImageRepository.findById(imageId).orElseThrow(
                 () -> new ResourceNotFoundException(String.format(IMAGE_NOT_FOUND_MSG, imageId)));
@@ -97,6 +65,10 @@ public class AgentService {
     public static List<AgentDTO> fetchAllAgents(){
         return agentRepository.findAllAgent();
     }
->>>>>>> main
 
+
+    
+    
+    
+    
 }
