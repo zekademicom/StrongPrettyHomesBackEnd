e707bab4a3aae64cde37c5f07126679910a3c5b9
Zekademi Bilişim & Yazılım <106998412+zekademicom@users.noreply.github.com>
2022-06-11 13:34:27 +0300

    Update AgentRepository.java

--- src/main/java/com/zekademi/strongprettyhomes/repository/AgentRepository.java
+++ src/main/java/com/zekademi/strongprettyhomes/repository/AgentRepository.java
@@ -3,6 +3,7 @@ package com.zekademi.strongprettyhomes.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+@Transactional
 @Repository
 public interface AgentRepository extends JpaRepository<Agent, Long> {
 }
