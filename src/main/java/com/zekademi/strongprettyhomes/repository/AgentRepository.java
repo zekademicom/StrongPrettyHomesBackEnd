7992c6a99b6eaa0e830ae78db1e480597d9e9116
severHatice <95497761+severHatice@users.noreply.github.com>
2022-06-11 12:22:02 +0200

    transaction

--- src/main/java/com/zekademi/strongprettyhomes/repository/AgentRepository.java
+++ src/main/java/com/zekademi/strongprettyhomes/repository/AgentRepository.java
@@ -5,10 +5,12 @@ import com.zekademi.strongprettyhomes.dto.AgentDTO;
 import com.zekademi.strongprettyhomes.exception.ResourceNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Repository
+@Transactional
 public interface AgentRepository extends JpaRepository<Agent, Long> {
     Optional<AgentDTO> findByIdOrderById(Long id) throws ResourceNotFoundException;
 }
