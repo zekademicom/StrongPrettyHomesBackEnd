8e4b81fef274d0729aa7e237d02573fb1c071354
theLostCode <lostcoding11@ghmail.com>
2022-06-15 15:27:17 +0300

    tour request repo created

---
+++ src/main/java/com/zekademi/strongprettyhomes/repository/TourRequestRepository.java
@@ -0,0 +1,16 @@
+package com.zekademi.strongprettyhomes.repository;
+
+import com.zekademi.strongprettyhomes.domain.TourRequest;
+import com.zekademi.strongprettyhomes.domain.User;
+import com.zekademi.strongprettyhomes.dto.TourRequestDTO;
+import com.zekademi.strongprettyhomes.exception.ResourceNotFoundException;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.client.ResourceAccessException;
+
+import java.util.Optional;
+
+public interface TourRequestRepository extends JpaRepository<TourRequest, Long> {
+    Optional<TourRequestDTO> findByIdOrderById(Long id) throws ResourceNotFoundException;
+
+    Optional<TourRequestDTO> findByIdAndUserId(Long id, User user) throws ResourceNotFoundException;
+}
