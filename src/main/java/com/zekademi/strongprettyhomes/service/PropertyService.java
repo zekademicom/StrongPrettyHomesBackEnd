e84b515dc2903efd4ecaa5e45e208dc3f5bf3ef2
returnNull <91457054+nullpointerexception11@users.noreply.github.com>
2022-06-15 15:33:08 +0300

    Update PropertyService.java

--- src/main/java/com/zekademi/strongprettyhomes/service/PropertyService.java
+++ src/main/java/com/zekademi/strongprettyhomes/service/PropertyService.java
@@ -58,11 +58,6 @@ public class PropertyService {
         Agent agent = agentRepository.findById(agentId).get();
         PropertyDetail propertyDetail = propertyDetailRepository.findById(detailId).get();
 
-//        Property property1 = propertyRepository.getById(id);
-//        if (property1.getBuiltIn())
-//            throw new BadRequestException("You dont have permission to update property!");
-//        property.setBuiltIn(false);
-
         Set<PropertyDetail> propertyDetails = new HashSet<PropertyDetail>();
         propertyDetails.add(propertyDetail);
 
@@ -73,45 +68,12 @@ public class PropertyService {
     }
 
      public void removeById(Long id) throws ResourceNotFoundException {
-       // Property property = propertyRepository.findById(id).orElseThrow(() ->
-              // new ResourceNotFoundException(String.format(PROPERTY_NOT_FOUND_MSG, id)));
-
-      //  if (property.getBuiltIn()) {
-       //     throw new BadRequestException("You dont have permission to delete property!");
-      //  }
-
+     
       // boolean reservationExist = propertyRepository.existsByProperty(property);
 
 //       if (reservationExist){
 //          throw new ResourceNotFoundException("Reservation(s) exist for property!"); }
         propertyRepository.deleteById(id);
    }
-
-//    // TODO: 11/06/2022 querylerle olusturuldu. birkac farkli yontem var. deneyip gorecegiz :)
-//    public List<PropertyDTO> searchList(String title, String type, String status, String bedrooms, String bathrooms,
-//                                        String country, String city, String district, double price1, double price2) {
-//
-//        if (title != null || type != null || status != null || bedrooms != null ||
-//                bathrooms != null || country != null || city != null || district != null
-//                || price1 != 0 || price2 != 0)
-//            return propertyRepository.findByTitleAndTypeAndStatusAndBedroomsAndBathroomsAndCountryAndCityAndDistrictAAndPriceBetween
-//                    (title, type, status, bedrooms, bathrooms, country, city, district, price1, price2);
-//
-//        /** String keyword, double price1, double price2*/
-////        if (keyword != null && price1 == 0 && price2 == 0) {
-////            return propertyRepository.search(keyword);
-////        }
-////        if (price1 != 0 && price2 != 0) {
-////            if (price2 >= price1) return propertyRepository.findByPriceBetween(price1, price2);
-////            else throw new BadRequestException(String.format(PRICE_DOESNT_MATCH, price1, price2));
-////        }
-////        if (price1 != 0 && price2 == 0) {
-////            return propertyRepository.findByPriceGreaterThanEqual(price1);
-////        }
-////        if (price1 == 0 && price2 != 0) {
-////            return propertyRepository.findByPriceLessThanEqual(price2);
-////        }
-//        return propertyRepository.findAllProperty();
-//    }
 
 }
