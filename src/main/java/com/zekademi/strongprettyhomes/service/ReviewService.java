8be1c6a704d09f95f767d33e38bf613182a76d1c
Burhan Bediz <94639299+BurhanBediz@users.noreply.github.com>
2022-06-16 11:34:37 +0300

    Update ReviewService.java

--- src/main/java/com/zekademi/strongprettyhomes/service/ReviewService.java
+++ src/main/java/com/zekademi/strongprettyhomes/service/ReviewService.java
@@ -9,9 +9,20 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 @Service
 public class ReviewService {
-     private ReviewRepository reviewRepository;
+     
+    private final PropertyRepository propertyRepository;
+    private final ReviewRepository reviewRepository;
+    private final UserRepository userRepository;
+    private final static String PROPERTY_NOT_FOUND_MSG = "property with id %d not found";
+    private final static String USER_NOT_FOUND_MSG = "user with id %d not found";
+    private final static String REVIEW_NOT_FOUND_MSG = "Review with id %d not found";
 
-     private final static String REVIEW_NOT_FOUND_MSG = "Review with id %d not found";
+    public List<ReviewDTO> findAllByPropertyId(Long propertyId) {
+
+        Property property = propertyRepository.findById(propertyId).orElseThrow(() ->
+                new ResourceNotFoundException(String.format(PROPERTY_NOT_FOUND_MSG, propertyId)));
+        return reviewRepository.findAllByUserId(property);
+    }
 
 //deneme
 
