13d650c32182c16467efe4fab51a3806ad8299ea
Burhan Bediz <94639299+BurhanBediz@users.noreply.github.com>
2022-06-16 22:34:00 +0300

    Update ReviewRepository.java
    Review Repository Updated

--- src/main/java/com/zekademi/strongprettyhomes/repository/ReviewRepository.java
+++ src/main/java/com/zekademi/strongprettyhomes/repository/ReviewRepository.java
@@ -3,10 +3,15 @@ package com.zekademi.strongprettyhomes.repository;
 import com.zekademi.strongprettyhomes.domain.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
+@Repository
+@Transactional
 public interface ReviewRepository extends JpaRepository<Review,Long> {
 
+    List<ReviewDTO> findAllByUserId(Property propertyId);
 
+    Optional<ReviewDTO> findByIdAndUserId(Long id, User user);
+
+    Optional<ReviewDTO> findByIdOrderById(Long id) throws ResourceNotFoundException;
 
 
 
