956351855c8184400dd2fc6a0c0bc907139b153c
returnNull <91457054+nullpointerexception11@users.noreply.github.com>
2022-06-19 15:20:11 +0300

    Update TourRequestController.java

--- src/main/java/com/zekademi/strongprettyhomes/controller/TourRequestController.java
+++ src/main/java/com/zekademi/strongprettyhomes/controller/TourRequestController.java
@@ -89,4 +89,13 @@ public class TourRequestController {
         map.put("Request status updated successfully", true);
         return new ResponseEntity<>(map, HttpStatus.OK);
     }
+    
+    @DeleteMapping("/admin/{id}/auth")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Map<String, Boolean>> deleteReservation(@PathVariable Long id){
+        tourRequestService.removeById(id);
+        Map<String, Boolean> map = new HashMap<>();
+        map.put("success", true);
+        return new ResponseEntity<>(map, HttpStatus.OK);
+    }
 }
