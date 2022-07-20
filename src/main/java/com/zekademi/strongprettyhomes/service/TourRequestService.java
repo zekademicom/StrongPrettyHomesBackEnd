6d19b8fe18002ec77d0cd034cb7f4b205f3da23a
Yildiz Aydogan <94450275+YildizAydogan@users.noreply.github.com>
2022-07-20 21:18:46 +0200

    Update TourRequestService.java

--- src/main/java/com/zekademi/strongprettyhomes/service/TourRequestService.java
+++ src/main/java/com/zekademi/strongprettyhomes/service/TourRequestService.java
@@ -142,6 +142,17 @@ public class TourRequestService {
         if (!reservationExists) throw new ResourceNotFoundException("reservation does not exist");
         tourRequestRepository.deleteById(id);
     }
+    
+     public void removeUserTourRequestById(Long userId, Long id) {
+        User user = userRepository.findById(userId).orElseThrow(() ->
+               new ResourceNotFoundException(String.format(USER_NOT_FOUND_MSG, userId)));
+        Optional<TourRequestDTO> tours = tourRequestRepository.findTour(userId,id);
+    //      if(reviews.isEmpty()){
+    //        throw new BadRequestException("You are unauthorized to delete this review!");
+    //     }
+        tourRequestRepository.deleteUserTourRequestById(id,user);
+
+    }
 
     public List<TourRequestDTO> fetchAllTourRequest() {
         return tourRequestRepository.findAllBy();
